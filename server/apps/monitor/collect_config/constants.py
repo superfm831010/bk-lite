0b7a2a9366af8b707b79d4275a1fce0b298b4580
baiyufei <baiyfyx@163.com>
2025-05-14 16:47:35 +0800

    feature-优化并补充jmx类型的采集插件

--- server/apps/monitor/collect_config/constants.py
+++ server/apps/monitor/collect_config/constants.py
@@ -9,9 +9,16 @@ ONLY_CHILD_CONFIG = {
     ("Telegraf", "middleware"): "toml",
     ("Telegraf", "docker"): "toml",
     ("Telegraf", "database"): "toml",
-    ("Telegraf", "vmware"): "toml",
+    ("Telegraf", "http"): "toml",
     ("Telegraf", "jmx"): "toml",
 }
 CONFIG_AND_CHILD_CONFIG = {
-    ("JMX-JVM", "jmx"): "yaml",
+    ("ActiveMQ-JMX", "jmx"): "yaml",
+    ("JBoss-JMX", "jmx"): "yaml",
+    ("Jetty-JMX", "jmx"): "yaml",
+    ("TongWeb6-JMX", "jmx"): "yaml",
+    ("TongWeb7-JMX", "jmx"): "yaml",
+    ("WebLogic-JMX", "jmx"): "yaml",
+    ("JVM-JMX", "jmx"): "yaml",
+    ("Tomcat-JMX", "jmx"): "yaml",
 }
