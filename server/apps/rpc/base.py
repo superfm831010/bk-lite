26b236c28c560be6ba49af8d9f74171cced4f5cc
zhmf7408 <kido@canway.net>
2025-05-16 14:57:26 +0800

    feature 统一namespace, 取消参数记录

--- server/apps/rpc/base.py
+++ server/apps/rpc/base.py
@@ -1,11 +1,12 @@
 import asyncio
+import os
 
 import nats_client
 
 
 class RpcClient(object):
     def __init__(self):
-        namespace = "bk_lite"
+        namespace = os.getenv("NATS_NAMESPACE", "bk_lite")
         self.namespace = namespace
 
     def run(self, method_name, *args, **kwargs):
