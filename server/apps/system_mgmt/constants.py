ebe718ac45f92d53a6bf23a1bc07c060ef5dbe5b
zhmf7408 <kido@canway.net>
2025-05-16 14:17:49 +0800

    feature 系统管理改造

--- server/apps/system_mgmt/constants.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BUILT_IN_ROLES = {
-    f"default-roles-{os.getenv('KEYCLOAK_REALM')}",
-    "default-roles-master",
-    "uma_authorization",
-    "offline_access",
-}
--- server/apps/system_mgmt/viewset/group_viewset.py
+++ server/apps/system_mgmt/viewset/group_viewset.py
@@ -1,69 +1,51 @@
 from django.http import JsonResponse
-from rest_framework import viewsets
+from django.utils.translation import gettext as _
 from rest_framework.decorators import action
 
-from apps.system_mgmt.services.group_manage import GroupManage
+from apps.system_mgmt.models import Group, User
+from apps.system_mgmt.serializers.group_serializer import GroupSerializer
+from apps.system_mgmt.utils.group_utils import GroupUtils
+from apps.system_mgmt.utils.viewset_utils import ViewSetUtils
 
 
-class GroupViewSet(viewsets.ViewSet):
+class GroupViewSet(ViewSetUtils):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
     @action(detail=False, methods=["GET"])
     def search_group_list(self, request):
-        data = GroupManage().group_list(request.GET.dict())
-        return JsonResponse({"result": True, "data": data})
+        queryset = Group.objects.all()
+        # 构建嵌套组结构
+        groups_data = GroupUtils.build_group_tree(queryset)
+        return JsonResponse({"result": True, "data": groups_data})
 
     @action(detail=False, methods=["GET"])
     def get_detail(self, request):
-        data = GroupManage().group_retrieve(request.GET.get("group_id"))
-        return JsonResponse({"result": True, "data": data})
+        group = Group.objects.get(id=request.GET["group_id"])
+        return JsonResponse(
+            {"result": True, "data": {"name": group.name, "id": group.id, "parent_id": group.parent_id}}
+        )
 
     @action(detail=False, methods=["POST"])
     def create_group(self, request):
-        data = GroupManage().group_create(request.data)
+        params = request.data
+        group = Group.objects.create(
+            parent_id=params.get("parent_group_id", 0),
+            name=params["group_name"],
+        )
+        data = {"id": group.id, "name": group.name, "parent_id": group.parent_id, "subGroupCount": 0, "subGroups": []}
         return JsonResponse({"result": True, "data": data})
 
     @action(detail=False, methods=["POST"])
     def update_group(self, request):
-        GroupManage().group_update(request.data)
+        Group.objects.filter(id=request.data.get("group_id")).update(name=request.data.get("group_name"))
         return JsonResponse({"result": True})
 
     @action(detail=False, methods=["POST"])
     def delete_groups(self, request):
-        GroupManage().group_delete(request.data)
+        kwargs = request.data
+        users = User.objects.filter(group_list__contains=int(kwargs["id"]))
+        if users:
+            raise Exception(_("This group or sub groups has users, please remove the users first!"))
+        Group.objects.filter(id=int(kwargs["id"])).delete()
         return JsonResponse({"result": True})
-
-    #
-    # @action(detail=False, methods=["GET"])
-    # def get_users_in_group(self, request):
-    #     pk = request.GET.get("group_id")
-    #     data = GroupManage().group_users(pk)
-    #     return JsonResponse({"result": True, "data": data})
-    #
-    # @action(detail=False, methods=["POST"])
-    # def assign_group_users(self, request):
-    #     pk = request.data.get("group_id")
-    #     data = GroupManage().group_add_users(request.data, pk)
-    #     return JsonResponse({"result": True, "data": data})
-    #
-    # @action(detail=False, methods=["POST"])
-    # def unassigned_group_users(self, request):
-    #     pk = request.data.get("group_id")
-    #     data = GroupManage().group_remove_users(request.data, pk)
-    #     return JsonResponse({"result": True, "data": data})
-    #
-    # @action(detail=False, methods=["GET"])
-    # def get_roles_in_group(self, request):
-    #     pk = request.GET.get("group_id")
-    #     data = GroupManage().group_roles(pk)
-    #     return JsonResponse({"result": True, "data": data})
-    #
-    # @action(detail=False, methods=["POST"])
-    # def assign_group_roles(self, request):
-    #     pk = request.data.get("group_id")
-    #     data = GroupManage().group_add_roles(request.data, pk)
-    #     return JsonResponse({"result": True, "data": data})
-    #
-    # @action(detail=False, methods=["POST"])
-    # def unassigned_group_roles(self, request):
-    #     pk = request.data.get("group_id")
-    #     data = GroupManage().group_remove_roles(request.data, pk)
-    #     return JsonResponse({"result": True, "data": data})
