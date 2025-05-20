25ee16d0ddf6d84599b9bfbf9f2440e62970f969
QiuJia-layla <1406489435@qq.com>
2025-05-20 16:10:11 +0800

    feat: add application edit & user permission button

--- web/src/app/system-manager/(pages)/application/page.tsx
+++ web/src/app/system-manager/(pages)/application/page.tsx
@@ -2,20 +2,20 @@
 
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { message } from 'antd';
+import { message, Menu, Button, Modal } from 'antd';
 import { useTranslation } from '@/utils/i18n';
 import EntityList from '@/components/entity-list';
 import { useClientData } from '@/context/client';
 import { ClientData } from '@/types/index';
-// import ApplicationFormModal from '@/app/system-manager/components/application/modify-applicaiton';
+import ApplicationFormModal from '@/app/system-manager/components/application/modify-applicaiton';
 
 const ApplicationPage = () => {
   const { t } = useTranslation();
   const { getAll, loading } = useClientData();
   const [dataList, setDataList] = useState<ClientData[]>([]);
-  // const [modalVisible, setModalVisible] = useState(false);
-  // const [isEdit, setIsEdit] = useState(false);
-  // const [currentItem, setCurrentItem] = useState<ClientData | null>(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const [currentItem, setCurrentItem] = useState<ClientData | null>(null);
 
   const loadItems = async (searchTerm = '') => {
     try {
@@ -46,65 +46,65 @@ const ApplicationPage = () => {
 
   const router = useRouter();
 
-  // const handleAddNew = () => {
-  //   setCurrentItem(null);
-  //   setIsEdit(false);
-  //   setModalVisible(true);
-  // };
+  const handleAddNew = () => {
+    setCurrentItem(null);
+    setIsEdit(false);
+    setModalVisible(true);
+  };
 
-  // const handleEdit = (item: any) => {
-  //   setCurrentItem(item);
-  //   setIsEdit(true);
-  //   setModalVisible(true);
-  // };
+  const handleEdit = (item: any) => {
+    setCurrentItem(item);
+    setIsEdit(true);
+    setModalVisible(true);
+  };
 
-  // const handleDelete = () => {
-  //   Modal.confirm({
-  //     title: t('common.confirmDelete'),
-  //     content: t('common.deleteConfirmContent'),
-  //     okText: t('common.confirm'),
-  //     cancelText: t('common.cancel'),
-  //     onOk: async () => {
-  //       // try {
-  //       //   await deleteClient(item.id);
-  //       //   message.success(t('common.deleteSuccess'));
-  //       //   loadItems();
-  //       // } catch {
-  //       //   message.error(t('common.deleteFailed'));
-  //       // }
-  //     }
-  //   });
-  // };
+  const handleDelete = () => {
+    Modal.confirm({
+      title: t('common.confirmDelete'),
+      content: t('common.deleteConfirmContent'),
+      okText: t('common.confirm'),
+      cancelText: t('common.cancel'),
+      onOk: async () => {
+        // try {
+        //   await deleteClient(item.id);
+        //   message.success(t('common.deleteSuccess'));
+        //   loadItems();
+        // } catch {
+        //   message.error(t('common.deleteFailed'));
+        // }
+      }
+    });
+  };
 
-  // const handleModalClose = () => {
-  //   setModalVisible(false);
-  // };
+  const handleModalClose = () => {
+    setModalVisible(false);
+  };
 
-  // const handleFormSuccess = () => {
-  //   setModalVisible(false);
-  //   loadItems();
-  // };
+  const handleFormSuccess = () => {
+    setModalVisible(false);
+    loadItems();
+  };
 
-  // const getMenuActions = (item: any) => {
-  //   return (
-  //     <Menu>
-  //       <Menu.Item key="edit" onClick={() => handleEdit(item)}>
-  //         {t('common.edit')}
-  //       </Menu.Item>
-  //       {!item.is_build_in && (
-  //         <Menu.Item key="delete" onClick={() => handleDelete()}>
-  //           {t('common.delete')}
-  //         </Menu.Item>
-  //       )}
-  //     </Menu>
-  //   );
-  // };
+  const getMenuActions = (item: any) => {
+    return (
+      <Menu>
+        <Menu.Item key="edit" disabled onClick={() => handleEdit(item)}>
+          {t('common.edit')}
+        </Menu.Item>
+        {!item.is_build_in && (
+          <Menu.Item key="delete" onClick={() => handleDelete()}>
+            {t('common.delete')}
+          </Menu.Item>
+        )}
+      </Menu>
+    );
+  };
 
-  // const addButton = (
-  //   <Button type="primary" onClick={handleAddNew} className="ml-2">
-  //     {t('common.add')}
-  //   </Button>
-  // );
+  const addButton = (
+    <Button type="primary" onClick={handleAddNew} className="ml-2" disabled>
+      {t('common.add')}
+    </Button>
+  );
 
   return (
     <div className='w-full'>
@@ -113,11 +113,11 @@ const ApplicationPage = () => {
         loading={loading}
         onSearch={handleSearch}
         onCardClick={handleCardClick}
-        // menuActions={getMenuActions}
-        // operateSection={addButton}
+        menuActions={getMenuActions}
+        operateSection={addButton}
       />
       
-      {/* <ApplicationFormModal 
+      <ApplicationFormModal 
         visible={modalVisible}
         initialData={
           currentItem
@@ -133,7 +133,7 @@ const ApplicationPage = () => {
         isEdit={isEdit}
         onClose={handleModalClose}
         onSuccess={handleFormSuccess}
-      /> */}
+      />
     </div>
   );
 };
