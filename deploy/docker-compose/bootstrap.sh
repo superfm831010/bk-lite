9925f9eb7ba7292c9856f67f2e5242659b1835ef
garming <garming@msn.com>
2025-06-03 15:48:50 +0800

    fix: 修复部署bug，创建缺失目录

--- deploy/docker-compose/bootstrap.sh
+++ deploy/docker-compose/bootstrap.sh
@@ -225,6 +225,7 @@ if [ -f ./conf/nats/nats.conf ]; then
     log "WARNING" "nats.conf文件已存在，文件将被覆盖..."
 else
     log "INFO" "创建 nats.conf 文件..."
+    mkdir -p ./conf/nats
 fi
 
 cat > ./conf/nats/nats.conf <<EOF
