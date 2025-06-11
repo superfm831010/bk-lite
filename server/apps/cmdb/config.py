483b69ade52e5135333ecb577d5fa40bcff990f4
windyzhao <1129710372@qq.com>
2025-06-11 17:45:21 +0800

    minor-去除注视

--- server/apps/cmdb/config.py
+++ server/apps/cmdb/config.py
@@ -4,9 +4,9 @@
 # @Author: windyzhao
 from celery.schedules import crontab
 
-# CELERY_BEAT_SCHEDULE = {
-#     'sync_periodic_update_task_status': {
-#         'task': 'apps.cmdb.celery_tasks.sync_periodic_update_task_status',
-#         'schedule': crontab(minute='*/5'),
-#     },
-# }
+CELERY_BEAT_SCHEDULE = {
+    'sync_periodic_update_task_status': {
+        'task': 'apps.cmdb.celery_tasks.sync_periodic_update_task_status',
+        'schedule': crontab(minute='*/5'),
+    },
+}
