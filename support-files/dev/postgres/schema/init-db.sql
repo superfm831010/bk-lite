ca601df12f96331708bb9ccd91edff2f526198d0
umaru <15875339926@139.com>
2025-06-22 18:40:41 +0800

    add mlflow support

--- support-files/dev/postgres/schema/init-db.sql
+++ support-files/dev/postgres/schema/init-db.sql
@@ -1,10 +1,2 @@
--- 创建所需的数据库
 CREATE DATABASE bklite;
-CREATE DATABASE airflow;
 CREATE DATABASE mlflow;
-
-
--- 为每个数据库设置默认用户权限
-GRANT ALL PRIVILEGES ON DATABASE airflow TO postgres;
-GRANT ALL PRIVILEGES ON DATABASE mlflow TO postgres;
-GRANT ALL PRIVILEGES ON DATABASE mlflow TO postgres;
