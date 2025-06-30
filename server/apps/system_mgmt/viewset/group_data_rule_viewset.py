f995d5d0c590382bf5abea0c99a6bef745cf9687
zhmf7408 <kido@canway.net>
2025-06-30 15:17:00 +0800

    feature: enhance app data retrieval and module listing functionality

--- server/apps/system_mgmt/viewset/group_data_rule_viewset.py
+++ server/apps/system_mgmt/viewset/group_data_rule_viewset.py
@@ -43,11 +43,10 @@ class GroupDataRuleViewSet(viewsets.ModelViewSet):
     @HasPermission("data_permission-View")
     def get_app_data(self, request):
         params = request.GET.dict()
-        client_map = {"opspilot": OpsPilot, "system-manager": SystemMgmt, "node": NodeMgmt}
-        app = params.pop("app")
-        if app not in client_map.keys():
-            return JsonResponse({"result": False, "message": _("APP not found")})
-        client = client_map[app]()
+        try:
+            client = self.get_client(params)
+        except Exception as e:
+            return JsonResponse({"result": False, "message": str(e)})
         fun = getattr(client, "get_module_data", None)
         if fun is None:
             return JsonResponse({"result": False, "message": _("Module not found")})
@@ -55,3 +54,31 @@ class GroupDataRuleViewSet(viewsets.ModelViewSet):
         params["page_size"] = int(params.get("page_size", "10"))
         return_data = fun(**params)
         return JsonResponse({"result": True, "data": return_data})
+
+    @action(methods=["GET"], detail=False)
+    @HasPermission("data_permission-View")
+    def get_app_module(self, request):
+        params = request.GET.dict()
+        try:
+            client = self.get_client(params)
+        except Exception as e:
+            return JsonResponse({"result": False, "message": str(e)})
+        fun = getattr(client, "get_module_list", None)
+        if fun is None:
+            return JsonResponse({"result": False, "message": _("Module not found")})
+        return_data = fun()
+        for i in return_data:
+            i["display_name"] = _(i["display_name"])
+            if "children" in i:
+                for child in i["children"]:
+                    child["display_name"] = _(child["display_name"])
+        return JsonResponse({"result": True, "data": return_data})
+
+    @staticmethod
+    def get_client(params):
+        client_map = {"opspilot": OpsPilot, "system-manager": SystemMgmt, "node": NodeMgmt}
+        app = params.pop("app")
+        if app not in client_map.keys():
+            raise Exception(_("APP not found"))
+        client = client_map[app]()
+        return client
