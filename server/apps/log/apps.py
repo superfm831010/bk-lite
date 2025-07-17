fb0feb56eb8ae177398734fde7377bda8bcdcd96
baiyufei <baiyfyx@163.com>
2025-07-17 17:07:44 +0800

    bugfix-修复数据流初始化问题

--- server/apps/log/apps.py
+++ server/apps/log/apps.py
@@ -1,17 +1,6 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
-
-
-def init_stream(**kwargs):
-    # 初始化默认数据流default
-    from apps.log.models import Stream
-    if not Stream.objects.filter(id='default').exists():
-        Stream.objects.create(id='default', name='Default', created_by="system", updated_by="system")
 
 
 class LogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.log'
-
-    def ready(self):
-        post_migrate.connect(init_stream)
--- server/apps/log/management/commands/log_init.py
+++ server/apps/log/management/commands/log_init.py
@@ -4,6 +4,12 @@ from apps.core.logger import log_logger as logger
 from apps.log.plugins.plugin_migrate import migrate_collector, migrate_collect_type
 
 
+def init_stream():
+    from apps.log.models import Stream
+    if not Stream.objects.filter(id='default').exists():
+        Stream.objects.create(id='default', name='Default', created_by="system", updated_by="system")
+
+
 class Command(BaseCommand):
     help = "日志插件初始化命令"
 
@@ -12,3 +18,7 @@ class Command(BaseCommand):
         migrate_collector()
         migrate_collect_type()
         logger.info("日志插件初始化完成！")
+
+        logger.info("初始化默认数据流开始！")
+        init_stream()
+        logger.info("默认数据流初始化完成！")
