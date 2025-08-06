a53395bbce977a2bf9a6367d07d8f50ac2286035
baiyufei <baiyfyx@163.com>
2025-08-06 11:23:44 +0800

    feature-日志告警策略

--- server/apps/log/urls.py
+++ server/apps/log/urls.py
@@ -3,6 +3,7 @@ from rest_framework import routers
 from apps.log.views.node import NodeViewSet
 from apps.log.views.search import LogSearchViewSet
 from apps.log.views.stream import StreamViewSet
+from apps.log.views.policy import PolicyViewSet, AlertViewSet, EventViewSet, EventRawDataViewSet
 
 from apps.log.views.collect_config import (
     CollectTypeViewSet,
@@ -18,5 +19,11 @@ router.register(r'collect_configs', CollectConfigViewSet, basename='collect_conf
 router.register(r'node_mgmt', NodeViewSet, basename='node_mgmt')
 router.register(r'streams', StreamViewSet, basename='stream')
 router.register(r'search', LogSearchViewSet, basename='log_search')
+
+# 策略相关路由
+router.register(r'policy', PolicyViewSet, basename='policy')
+router.register(r'alert', AlertViewSet, basename='alert')
+router.register(r'event', EventViewSet, basename='event')
+router.register(r'event_raw_data', EventRawDataViewSet, basename='event_raw_data')
 
 urlpatterns = router.urls
