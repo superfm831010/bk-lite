5843827ba4783c01cd3001d3b566db76a8f961d1
wangkaituo <1811821770@qq.com>
2025-08-08 13:40:30 +0800

    feature:category数据初始化修改

--- server/apps/playground/init_constant.py
+++ server/apps/playground/init_constant.py
@@ -3,7 +3,7 @@ CATEGORY_INIT_DATA = [
         "name": "异常检测",
         "description": "异常检测",
         "parent": None,
-        "created_by": "admin",
-        "updated_by": "admin"
+        "created_by": "system",
+        "updated_by": "system"
     }
 ]
--- server/apps/playground/management/commands/category_init.py
+++ server/apps/playground/management/commands/category_init.py
@@ -4,6 +4,7 @@ from django.core.management import BaseCommand
 
 from apps.playground.models import PlayGroundCategory
 from apps.playground.init_constant import CATEGORY_INIT_DATA
+
 class Command(BaseCommand):
     help = "初始化分类"
 
@@ -11,16 +12,20 @@ class Command(BaseCommand):
         logging.info("开始初始化分类")
         try:
             for item in CATEGORY_INIT_DATA:
-                obj,created = PlayGroundCategory.objects.get_or_create(
-                    name=item["name"],
-                    description=item["description"],
-                    parent=item["parent"],
-                    created_by=item["created_by"],
-                    updated_by = item["updated_by"]
-                )
-                if created:
-                    logging.info(f"初始化分类成功: {item['name']}")
+                if not item["name"] or "name" not in item:
+                    logging.error("分类名称不能为空")
+                    continue
                 else:
-                    logging.info(f"分类已存在: {item['name']}")
+                    _, created = PlayGroundCategory.objects.get_or_create(
+                        name=item["name"],
+                        description=item.get("description", None),
+                        parent=item.get("parent", None),
+                        created_by=item.get("created_by","system"),
+                        updated_by=item.get("updated_by","system")
+                    )
+                    if created:
+                        logging.info(f"初始化分类成功: {item['name']}")
+                    else:
+                        logging.info(f"分类已存在: {item['name']}")
         except Exception as e:
             logging.error(f"初始化分类失败: {e}")
