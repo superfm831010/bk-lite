d550cd97e8f731ee21984bf007d315f9a30d069f
umaru <15875339926@139.com>
2025-08-13 17:46:49 +0800

    refactor: refactor server startup logic into dedicated module
    - Move the server initialization logic from `server.py` to a new `dev.py` file.
    - Remove redundant `__main__` block and related code from `server.py`.
    
    Signed-off-by: umaru <15875339926@139.com>

--- metis/server.py
+++ metis/server.py
@@ -117,9 +117,3 @@ def bootstrap() -> Sanic:
     return app
 
 
-if __name__ == "__main__":
-    app = bootstrap()
-
-    app.run(
-        host="0.0.0.0", port=18082, debug=True, auto_reload=False, single_process=True
-    )
