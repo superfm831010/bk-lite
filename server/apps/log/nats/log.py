71d93ebad616c14bc705824c7821e12a8031bac1
baiyufei <baiyfyx@163.com>
2025-08-16 19:58:46 +0800

    feature-日志模块开运营分析使用的nats方法

---
+++ server/apps/log/nats/log.py
@@ -0,0 +1,20 @@
+import nats_client
+from apps.log.utils.query_log import VictoriaMetricsAPI
+
+
+@nats_client.register
+def log_search(query, start_time, end_time, limit=10):
+    """搜索日志"""
+    vm_api = VictoriaMetricsAPI()
+    data = vm_api.query(query, start_time, end_time, limit)
+
+    return data
+
+
+@nats_client.register
+def log_hits(query, start_time, end_time, field, fields_limit=5, step="5m"):
+    """搜索日志命中数"""
+    vm_api = VictoriaMetricsAPI()
+    data = vm_api.hits(query, start_time, end_time, field, fields_limit, step)
+
+    return data
