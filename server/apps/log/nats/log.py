fe9f53c2e3222dd1ddcd6e02cf83377f88ad85b7
windyzhao <1129710372@qq.com>
2025-08-18 14:19:00 +0800

    feature-修改nats接口的返回体和做基类的抽离

--- server/apps/log/nats/log.py
+++ server/apps/log/nats/log.py
@@ -7,8 +7,7 @@ def log_search(query, start_time, end_time, limit=10):
     """搜索日志"""
     vm_api = VictoriaMetricsAPI()
     data = vm_api.query(query, start_time, end_time, limit)
-
-    return data
+    return {"result": True, "data": data, "message": ""}
 
 
 @nats_client.register
@@ -16,5 +15,4 @@ def log_hits(query, start_time, end_time, field, fields_limit=5, step="5m"):
     """搜索日志命中数"""
     vm_api = VictoriaMetricsAPI()
     data = vm_api.hits(query, start_time, end_time, field, fields_limit, step)
-
-    return data
+    return {"result": True, "data": data, "message": ""}
