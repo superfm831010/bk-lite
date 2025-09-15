c44b8729d4a22979505762d41203980c0e57238a
baiyufei <baiyfyx@163.com>
2025-09-15 14:57:33 +0800

    feature-查询条件保存功能

--- server/apps/log/views/search.py
+++ server/apps/log/views/search.py
@@ -1,11 +1,14 @@
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import action
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ViewSet, ModelViewSet
 
 from apps.core.utils.web_utils import WebUtils
 from apps.log.services.search import SearchService
 from apps.log.utils.log_group import LogGroupQueryBuilder
+from apps.log.models.log_group import SearchCondition
+from apps.log.serializers.log_group import SearchConditionSerializer
+from apps.log.filters.log_group import SearchConditionFilter
 
 
 class LogSearchViewSet(ViewSet):
@@ -128,3 +131,176 @@ class LogSearchViewSet(ViewSet):
             return WebUtils.response_error(error_msg)
 
         return SearchService.tail(query, log_groups)
+
+class SearchConditionViewSet(ModelViewSet):
+    """搜索条件管理ViewSet"""
+    queryset = SearchCondition.objects.all()
+    serializer_class = SearchConditionSerializer
+    filterset_class = SearchConditionFilter
+
+    def get_queryset(self):
+        """根据当前组织过滤查询集"""
+        current_team = self.request.COOKIES.get("current_team")
+        if current_team:
+            return SearchCondition.objects.filter(organization=int(current_team))
+        return SearchCondition.objects.none()
+
+    @swagger_auto_schema(
+        operation_description="创建搜索条件",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "name": openapi.Schema(type=openapi.TYPE_STRING, description="搜索条件名称"),
+                "condition": openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    description="搜索条件配置",
+                    example={
+                        "query": "error",
+                        "log_groups": ["log-group-1", "log-group-2"],
+                        "time_range": {"start": "2024-01-01T00:00:00Z", "end": "2024-01-02T00:00:00Z"},
+                        "filters": {"level": "error"}
+                    }
+                ),
+            },
+            required=["name", "condition"]
+        ),
+        tags=['SearchCondition']
+    )
+    def create(self, request, *args, **kwargs):
+        """创建搜索条件"""
+        current_team = request.COOKIES.get("current_team")
+        if not current_team:
+            return WebUtils.response_error("当前组织信息不存在，请重新登录")
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # 设置创建者和组织
+        serializer.validated_data['created_by'] = request.user.username
+        serializer.validated_data['organization'] = int(current_team)
+
+        search_condition = serializer.save()
+
+        return WebUtils.response_success({
+            "id": search_condition.id,
+            "name": search_condition.name,
+            "message": "搜索条件创建成功"
+        })
+
+    @swagger_auto_schema(
+        operation_description="获取搜索条件列表",
+        manual_parameters=[
+            openapi.Parameter('name', openapi.IN_QUERY, description="按名称过滤", type=openapi.TYPE_STRING),
+            openapi.Parameter('created_by', openapi.IN_QUERY, description="按创建者过滤", type=openapi.TYPE_STRING),
+        ],
+        tags=['SearchCondition']
+    )
+    def list(self, request, *args, **kwargs):
+        """获取搜索条件列表"""
+        current_team = request.COOKIES.get("current_team")
+        if not current_team:
+            return WebUtils.response_error("当前组织信息不存在，请重新登录")
+
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # 分页处理
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return WebUtils.response_success(serializer.data)
+
+    @swagger_auto_schema(
+        operation_description="获取搜索条件详情",
+        tags=['SearchCondition']
+    )
+    def retrieve(self, request, *args, **kwargs):
+        """获取搜索条件详情"""
+        current_team = request.COOKIES.get("current_team")
+        if not current_team:
+            return WebUtils.response_error("当前组织信息不存在，请重新登录")
+
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return WebUtils.response_success(serializer.data)
+
+    @swagger_auto_schema(
+        operation_description="更新搜索条件",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "name": openapi.Schema(type=openapi.TYPE_STRING, description="搜索条件名称"),
+                "condition": openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    description="搜索条件配置",
+                    example={
+                        "query": "error",
+                        "log_groups": ["log-group-1", "log-group-2"],
+                        "time_range": {"start": "2024-01-01T00:00:00Z", "end": "2024-01-02T00:00:00Z"},
+                        "filters": {"level": "error"}
+                    }
+                ),
+            }
+        ),
+        tags=['SearchCondition']
+    )
+    def update(self, request, *args, **kwargs):
+        """更新搜索条件"""
+        current_team = request.COOKIES.get("current_team")
+        if not current_team:
+            return WebUtils.response_error("当前组织信息不存在，请重新登录")
+
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+
+        # 设置更新者（不允许修改组织）
+        serializer.validated_data['updated_by'] = request.user.username
+
+        search_condition = serializer.save()
+
+        return WebUtils.response_success({
+            "id": search_condition.id,
+            "name": search_condition.name,
+            "message": "搜索条件更新成功"
+        })
+
+    @swagger_auto_schema(
+        operation_description="部分更新搜索条件",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "name": openapi.Schema(type=openapi.TYPE_STRING, description="搜索条件名称"),
+                "condition": openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    description="搜索条件配置"
+                ),
+            }
+        ),
+        tags=['SearchCondition']
+    )
+    def partial_update(self, request, *args, **kwargs):
+        """部分更新搜索条件"""
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="删除搜索条件",
+        tags=['SearchCondition']
+    )
+    def destroy(self, request, *args, **kwargs):
+        """删除搜索条件"""
+        current_team = request.COOKIES.get("current_team")
+        if not current_team:
+            return WebUtils.response_error("当前组织信息不存在，请重新登录")
+
+        instance = self.get_object()
+        search_condition_name = instance.name
+        instance.delete()
+
+        return WebUtils.response_success({
+            "message": f"搜索条件 '{search_condition_name}' 删除成功"
+        })
