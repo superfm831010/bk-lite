ae720bddf7b18f573b32081f5cdaa791a0dabacb
baiyufei <baiyfyx@163.com>
2025-09-23 18:16:34 +0800

    feature-弃用token初始化问题

--- server/apps/node_mgmt/management/commands/node_init.py
+++ server/apps/node_mgmt/management/commands/node_init.py
@@ -23,8 +23,8 @@ class Command(BaseCommand):
         collector_init()
         logger.info("初始化采集器完成！")
 
-        if os.getenv('SIDECAR_INIT_TOKEN'):
-            logger.info("初始化sidecar token！")
-            SidecarApiToken.objects.get_or_create(token=os.getenv('SIDECAR_INIT_TOKEN'),
-                                                  defaults={"created_by": "system", "updated_by": "system"})
-            logger.info("初始化sidecar token完成！")
+        # if os.getenv('SIDECAR_INIT_TOKEN'):
+        #     logger.info("初始化sidecar token！")
+        #     SidecarApiToken.objects.get_or_create(token=os.getenv('SIDECAR_INIT_TOKEN'),
+        #                                           defaults={"created_by": "system", "updated_by": "system"})
+        #     logger.info("初始化sidecar token完成！")
