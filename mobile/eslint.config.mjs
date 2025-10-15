9d6af2add3a80e62e87f6ab6699c19fd1abb203d
hongxixixi <hollyli@canway.net>
2025-10-15 17:17:37 +0800

    feat: enhance compatibility and layout improvements

--- mobile/eslint.config.mjs
+++ mobile/eslint.config.mjs
@@ -6,54 +6,45 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**", 
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-      "src-tauri/target/**"
-    ]
-  },
-
-  ...compat.extends("next", "plugin:@typescript-eslint/recommended"),
-
-  {
-    rules: {
-      "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
-      "@typescript-eslint/no-var-requires": "off",
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": ["error", {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_"
-      }],
-      "@typescript-eslint/no-unused-expressions": ["error", {
-        allowShortCircuit: true
-      }],
-      "no-console": "off",
-      "prefer-const": "error",
-      "no-debugger": "error",
-      "indent": ["error", 2, {
-        SwitchCase: 1,
-        flatTernaryExpressions: false,
-        ignoredNodes: [
-          "PropertyDefinition[decorators]",
-          "TSUnionType",
-          "FunctionExpression[params]:has(Identifier[decorators])"
+    {
+        ignores: [
+            "node_modules/**",
+            ".next/**",
+            "out/**",
+            "build/**",
+            "next-env.d.ts",
+            "src-tauri/target/**"
         ]
-      }],
-      "react-hooks/exhaustive-deps": "off",
-      "react/display-name": "warn",
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-img-element": "off"
+    },
+
+    ...compat.extends("next", "plugin:@typescript-eslint/recommended"),
+
+    {
+        rules: {
+            "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+            "@typescript-eslint/no-var-requires": "off",
+            "@typescript-eslint/no-non-null-assertion": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-unused-vars": ["error", {
+                argsIgnorePattern: "^_",
+                varsIgnorePattern: "^_"
+            }],
+            "@typescript-eslint/no-unused-expressions": ["error", {
+                allowShortCircuit: true
+            }],
+            "no-console": "off",
+            "prefer-const": "error",
+            "no-debugger": "error",
+            "react-hooks/exhaustive-deps": "off",
+            "react/display-name": "warn",
+            "react/no-unescaped-entities": "off",
+            "@next/next/no-img-element": "off"
+        }
     }
-  }
 ];
 
 export default eslintConfig;
