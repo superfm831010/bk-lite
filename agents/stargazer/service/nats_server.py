f35b04c1683c410f23c6d756c38f5154dfa078b5
baiyufei <baiyfyx@163.com>
2025-10-21 10:02:03 +0800

    feat-nats支持tls

--- agents/stargazer/service/nats_server.py
+++ agents/stargazer/service/nats_server.py
@@ -2,23 +2,23 @@
 # @File: nats_server.py
 # @Time: 2025/4/25 17:04
 # @Author: windyzhao
-import json
 from sanic.log import logger
 from service.collect_service import CollectService
-from core.nats_instance import register_handler
+from core.nats import register_handler
 
 
-# 注册 list_regions 处理器
 @register_handler("list_regions")
 async def list_regions(data):
-    logger.info(f"list_regions Received data: {data}")
-    kwargs = data["kwargs"]
+    """处理 list_regions 请求"""
+    logger.info(f"list_regions received: {data}")
+    kwargs = data.get("kwargs", {})
     collect_service = CollectService(kwargs)
     regions = collect_service.list_regions()
-    logger.info(f"list_regions regions data: {regions}")
-    return regions
+    return {"regions": regions}
 
 
-async def test_connection(request):
-    data = request.json
-    return json.dumps({"result": True, "data": data})
+@register_handler("test_connection")
+async def test_connection(data):
+    """测试连接"""
+    logger.info(f"test_connection received: {data}")
+    return {"result": True, "data": data}
